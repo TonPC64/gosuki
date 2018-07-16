a3dc76c0adab999782528e0208fd3eef367bc538
Chanwit Piromplad <kingkong2103@gmail.com>
2018-07-16 17:29:13 +0700

    add done

--- main.go
+++ main.go
@@ -2,42 +2,123 @@ package main
 
 import (
 	"fmt"
+	"strings"
 )
 
-func init() {
-	println("init func()")
-}
-
 func main() {
-	number := []int{}
-	for i := 1; i <= 100; i++ {
-		number = append(number, i)
-	}
-	sum, avg := calcOnlyPrime(number...)
-	fmt.Printf("sum = %d, avg = %f\n", sum, avg)
+	wc := NewWordCount()
+	wc = wc.AddWord("I am")
+	wc = wc.AddWord("I am")
+	wc = wc.AddWord("am")
+	fmt.Println(wc.MaxWordCount())
+	fmt.Println(wc.MinWordCount())
+	fmt.Println(wc.LongestWord())
+	wc.PrintAll()
 }
 
-func calcOnlyPrime(i ...int) (int, float32) {
-	sum := 0
-	count := 0
-	for _, num := range i {
-		if checkPrime(num) {
-			sum += num
-			count++
-		}
-	}
-	avg := float32(sum) / float32(count)
-	return sum, avg
+type WordsCount map[string]WordCount
+
+type WordCount struct {
+	Count  int
+	Length int
 }
 
-func checkPrime(num int) bool {
-	if num <= 1 {
-		return false
-	}
-	for i := 2; i <= num/2; i++ {
-		if num%i == 0 {
-			return false
+func NewWordCount() WordsCount {
+	return make(WordsCount)
+}
+
+func (wc WordsCount) AddWord(s string) WordsCount {
+	for _, str := range strings.Fields(s) {
+		wc[str] = WordCount{
+			Count:  wc[str].Count + 1,
+			Length: len(str),
 		}
 	}
-	return true
+	return wc
+}
+
+func (wc WordsCount) MaxWordCount() ([]string, int) {
+	counts := []int{}
+	for _, v := range wc {
+		counts = append(counts, v.Count)
+	}
+	max := maxInts(counts...)
+	s := []string{}
+	for key, v := range wc {
+		if max == v.Count {
+			s = append(s, key)
+		}
+	}
+	return s, max
+}
+
+func (wc WordsCount) MinWordCount() ([]string, int) {
+	counts := []int{}
+	for _, v := range wc {
+		counts = append(counts, v.Count)
+	}
+	min := minInts(counts...)
+	s := []string{}
+	for key, v := range wc {
+		if min == v.Count {
+			s = append(s, key)
+		}
+	}
+	return s, min
+}
+
+func (wc WordsCount) LongestWord() ([]string, int) {
+	counts := []int{}
+	for _, v := range wc {
+		counts = append(counts, v.Length)
+	}
+	max := maxInts(counts...)
+	s := []string{}
+	for key, v := range wc {
+		if max == v.Length {
+			s = append(s, key)
+		}
+	}
+	return s, max
+}
+
+func (wc WordsCount) ShortestWord() ([]string, int) {
+	counts := []int{}
+	for _, v := range wc {
+		counts = append(counts, v.Length)
+	}
+	min := minInts(counts...)
+	s := []string{}
+	for key, v := range wc {
+		if min == v.Length {
+			s = append(s, key)
+		}
+	}
+	return s, min
+}
+
+func (wc WordsCount) PrintAll() {
+	for key, value := range wc {
+		fmt.Println("Key:", key, "Value:", value)
+	}
+}
+
+func maxInts(nums ...int) int {
+	maxNum := nums[0]
+	for _, num := range nums {
+		if num > maxNum {
+			maxNum = num
+		}
+	}
+	return maxNum
+}
+
+func minInts(nums ...int) int {
+	minNum := nums[0]
+	for _, num := range nums {
+		if num < minNum {
+			minNum = num
+		}
+	}
+	return minNum
 }
